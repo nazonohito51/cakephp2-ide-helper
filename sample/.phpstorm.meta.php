e0a02aaef80f3a8c0699549c663e91c7805bd0bf
SatoshiKawashima <nazonohito51@gmail.com>
2019-08-05 10:31:19 +0900

    update sample

--- sample/.phpstorm.meta.php
+++ sample/.phpstorm.meta.php
@@ -1,36 +1,17 @@
 <?php
 
-namespace PHPSTORM_META {  // we want to avoid the pollution
-	// this is a saner and self-documented format for PhpStorm 2016.2 and later
-	// Try QuickDoc on these "magic" functions, or even Go to definition!
-	override(
-		\ServiceLocatorInterface::get(0),         // method signature //argument number is ALWAYS 0 now.
-		map(
-			[ //map of argument value -> return type
-				"special" => \Exception::class,                //Reference target classes by ::class constant
-				\ExampleFactory::EXAMPLE_B => ExampleB::class,  // FYI, we can now support class constant argument values
-				\EXAMPLE_B => \ExampleB::class,              // and global constants too
-				//non-mapped value, e.g. $getByClassNameConst case above will be returned automatically
-			]
-		)
-	);
-
-	override(
-		\ClassRegistry::init(0),         // method signature //argument number is ALWAYS 0 now.
-		map(
-			[ //map of argument value -> return type
-				'Plugin.SomeModel1' => 'SomeModel1',                //Reference target classes by ::class constant
-				'SomeModel2' => 'SomeModel2',
-				\ExampleFactory::EXAMPLE_B => ExampleB::class,  // FYI, we can now support class constant argument values
-				\EXAMPLE_B => \ExampleB::class,              // and global constants too
-				//non-mapped value, e.g. $getByClassNameConst case above will be returned automatically
-			]
-		)
-	);
-
-	expectedArguments(
-		\AppModel::withRead(),
-		0,
-
-	);
+namespace PHPSTORM_META {
+    override(
+        \ClassRegistry::init(0),
+        map(
+            array(
+                'SomeModel1' => 'SomeModel1',
+                'SomeModel2' => 'SomeModel2',,
+                'Plugin1.SomeModel3' => 'SomeModel3',
+                'Plugin1.SomeModel4' => 'SomeModel4',
+                'Plugin2.SomeModel5' => 'SomeModel5',
+                'Plugin2.SomeModel6' => 'SomeModel6',
+            )
+        )
+    );
 }
