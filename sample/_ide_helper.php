d1047bbe1dcf888bbd6e9f28f2de8c4a0f88f226
nazonohito51 <nazonohito51@gmail.com>
2019-08-12 19:48:06 +0900

    add sample

---
+++ sample/_ide_helper.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CakePhp2IdeHelper;
+
+class SomeBehavior1
+{
+    /**
+     * @see \SomeBehavior1::someBehavior1Method1()
+     */
+    public function someBehavior1Method1(bool $arg1, bool $args2) : bool
+    {
+    }
+    /**
+     * @see \SomeBehavior1::someBehavior1Method2()
+     */
+    public function someBehavior1Method2(int $arg1, int $args2) : int
+    {
+    }
+    /**
+     * @see \SomeBehavior1::someBehavior1Method3()
+     */
+    public function someBehavior1Method3(string $arg1, string $args2) : string
+    {
+    }
+}
+class SomeBehavior2
+{
+    /**
+     * @see \SomeBehavior2::someBehavior2Method1()
+     */
+    public function someBehavior2Method1(bool $arg1, bool $args2) : bool
+    {
+    }
+    /**
+     * @see \SomeBehavior2::someBehavior2Method2()
+     */
+    public function someBehavior2Method2(int $arg1, int $args2) : int
+    {
+    }
+    /**
+     * @see \SomeBehavior2::someBehavior2Method3()
+     */
+    public function someBehavior2Method3(string $arg1, string $args2) : string
+    {
+    }
+}
+class SomeBehavior3
+{
+    /**
+     * @see \SomeBehavior3::someBehavior3Method1()
+     */
+    public function someBehavior3Method1(bool $arg1, bool $args2) : bool
+    {
+    }
+    /**
+     * @see \SomeBehavior3::someBehavior3Method2()
+     */
+    public function someBehavior3Method2(int $arg1, int $args2) : int
+    {
+    }
+    /**
+     * @see \SomeBehavior3::someBehavior3Method3()
+     */
+    public function someBehavior3Method3(string $arg1, string $args2) : string
+    {
+    }
+}
