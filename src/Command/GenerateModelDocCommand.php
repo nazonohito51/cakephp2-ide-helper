eefa4050d8252ef15bf258ff636c4d22e686a7e3
nazonohito51 <nazonohito51@gmail.com>
2019-08-12 19:38:04 +0900

    add GenerateModelDocCommand

--- src/Command/GenerateModelDocCommand.php
+++ src/Command/GenerateModelDocCommand.php
@@ -5,24 +5,23 @@ namespace CakePhp2IdeHelper\Command;
 
 use CakePhp2IdeHelper\Generator;
 
-class GenerateHelperCommand extends AbstractIdeHelperCommand
+class GenerateModelDocCommand extends AbstractIdeHelperCommand
 {
     protected function getCommandName(): string
     {
-        return 'generate:helper';
+        return 'generate:model';
     }
 
     protected function getCommandDescription(): string
     {
-        return 'generate _ide_helper.php';
+        return 'generate model phpdoc';
     }
 
     protected function generateFromGenerator(Generator $generator): int
     {
-        $ideHelperContent = $generator->generateIdeHelperContent();
-
-        $ideHelperFile = new \SplFileObject(getcwd() . '/_ide_helper.php', 'w');
-        $ideHelperFile->fwrite($ideHelperContent);
+        foreach ($generator->generateModelDocEntries() as $updateModelDocEntry) {
+            $updateModelDocEntry->update();
+        }
 
         return 0;
     }
